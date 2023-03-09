4677fa607ea339ed47a1583445d97ef6a92e2e5c
ivandjoh <ivandi.djoh@gmail.com>
2023-03-09 16:08:45 +0800

    add product CRUD!

--- main.py
+++ main.py
@@ -37,12 +37,15 @@ class Product(HashModel):
         database = redis
 
 
+# Routes
+# Health Check
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/envinfo")
+# Env Info
+@app.get("/api/v1/envinfo")
 async def read_envinfo():
     from config import get_settings
     settings = get_settings()
@@ -55,11 +58,43 @@ async def read_envinfo():
     }
 
 
-@app.get("/products")
+# Product Format
+def format(pk: str):
+    product = Product.get(pk)
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'description': product.description,
+        'price': product.price,
+        'quantity': product.quantity,
+    }
+
+
+# Get ALl Product Routes
+@app.get("/api/v1/products")
 async def get_all_products():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
 
 
-@app.post("/product")
+# Add Product Routes
+@app.post("/api/v1/product")
 async def create_product(product: Product):
     return product.save()
+
+
+# Get Single Product Routes
+@app.get("/api/v1/product/{pk}")
+async def get_product(pk: str):
+    return Product.get(pk)
+
+
+# Update Product Routes
+@app.put("/api/v1/product/{pk}")
+async def update_product(pk: str, product: Product):
+    return product.save()
+
+
+# Delete Product Routes
+@app.delete("/api/v1/product/{pk}")
+async def delete_product(pk: str):
+    return Product.delete(pk)
